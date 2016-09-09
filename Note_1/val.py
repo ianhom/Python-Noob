69a899b79e0c5840e5aab5eea134a362450991b4
Ian Hom <ianhom@163.com>
2016-09-09 22:52:04 +0800

    Create val.py

---
+++ Note_1/val.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+counter = 100 # 赋值整型变量
+miles = 1000.0 # 浮点型
+name = "John" # 字符串
+if counter == 100:
+    print counter;
+else:
+    print miles;
+print name;
+
+# 运行结果
+"""
+100
+John
+"""
