37dc5bf238481cbf98cc9a90a1954768568050c8
Ian Hom <ianhom@163.com>
2016-09-09 22:43:59 +0800

    Create hello.py

---
+++ Note_1/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python    
+# -*- coding: UTF-8 -*-
+# 上面的Python的解释器路径 
+print "Hello, World!";   # 打印Hello, World!  
+print "This is my first python code";
+
+# 运行结果
+"""
+Hello, World!
+This is my first python code
+"""
