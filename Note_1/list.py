6fa5998d52b7c2ecb887488db51b4ccd00cfa890
Ian Hom <ianhom@163.com>
2016-09-10 09:29:40 +0800

    Create list.py

---
+++ Note_1/list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+list = [ 'abcd', 123, 1.23, 'Ian' ]
+smalllist = [ 123, 'Jane' ]
+
+print list              # print all elements
+print list[0]           # print the first element
+print list[1:3]         # print the second one to the third one
+print list[2:]          # print the third and following elements
+print samlllist * 2     # print small list twice
+print list + smalllist  # print both list
+
