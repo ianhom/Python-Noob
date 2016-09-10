095e2c409af708fe3c7f6ef6570fa62ceaeab711
Ian Hom <ianhom@163.com>
2016-09-10 09:34:42 +0800

    Update list.py

--- Note_1/list.py
+++ Note_1/list.py
@@ -8,6 +8,15 @@ print list              # print all elements
 print list[0]           # print the first element
 print list[1:3]         # print the second one to the third one
 print list[2:]          # print the third and following elements
-print samlllist * 2     # print small list twice
+print smalllist * 2     # print small list twice
 print list + smalllist  # print both list
 
+# result
+""""
+['abcd', 123, 1.23, 'Ian']
+abcd
+[123, 1.23]
+[1.23, 'Ian']
+[123, 'Jane', 123, 'Jane']
+['abcd', 123, 1.23, 'Ian', 123, 'Jane']
+""""
