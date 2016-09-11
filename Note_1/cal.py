b99538714f04c87a3d679a9137c8e9f0571098e4
Ian Hom <ianhom@163.com>
2016-09-11 11:24:34 +0800

    Create cal.py

---
+++ Note_1/cal.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print "1 - c 的值为：", c
+
+c = a - b
+print "2 - c 的值为：", c 
+
+c = a * b
+print "3 - c 的值为：", c 
+
+c = a / b
+print "4 - c 的值为：", c 
+
+c = a % b
+print "5 - c 的值为：", c
+
+# 修改变量 a 、b 、c
+a = 2
+b = 3
+c = a**b 
+print "6 - c 的值为：", c
+
+a = 10
+b = 5
+c = a//b 
+print "7 - c 的值为：", c
+
+
+# result
+"""
+1 - c 的值为： 31
+2 - c 的值为： 11
+3 - c 的值为： 210
+4 - c 的值为： 2
+5 - c 的值为： 1
+6 - c 的值为： 8
+7 - c 的值为： 2
+"""
