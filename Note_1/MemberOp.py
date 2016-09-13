c905c14d3632bd5f4e00b5c11daf55790ef56f30
Ian Hom <ianhom@163.com>
2016-09-13 08:43:16 +0800

    Update MemberOp.py

--- Note_1/MemberOp.py
+++ Note_1/MemberOp.py
@@ -4,6 +4,7 @@
 a = 10
 b = 20
 list = [1, 2, 3, 4, 5 ];
+biao = [3, 4, 5, 6];
 
 if ( a in list ):
    print "1 - 变量 a 在给定的列表中 list 中"
@@ -21,10 +22,17 @@ if ( a in list ):
    print "3 - 变量 a 在给定的列表中 list 中"
 else:
    print "3 - 变量 a 不在给定的列表中 list 中"
-   
+
+# 修改变量 b 的值
+b = 1
+if ( b in biao):
+    print "b is in the biao"
+else:
+    print "b is NOT in the biao"
 # result
 """
 1 - 变量 a 不在给定的列表中 list 中
 2 - 变量 b 不在给定的列表中 list 中
 3 - 变量 a 在给定的列表中 list 中
+b is NOT in the biao
 """
