296bcd65e023bf66b8fb88c94be16ab96df54784
Ian Hom <ianhom@163.com>
2016-09-14 08:58:12 +0800

    Create while.py

---
+++ Note_1/while.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: UTF-8  -*-
+
+count = 0;
+while(count < 20):
+    print "The count is: ", count;
+    count = count + 1;
+print "end";
+
+# result
+"""
+The count is: 0
+The count is: 1
+The count is: 2
+The count is: 3
+The count is: 4
+The count is: 5
+The count is: 6
+The count is: 7
+The count is: 8
+The count is: 9
+The count is: 10
+The count is: 11
+The count is: 12
+The count is: 13
+The count is: 14
+The count is: 15
+The count is: 16
+The count is: 17
+The count is: 18
+The count is: 19
+end
+"""
