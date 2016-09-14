1d182cbf72f88ea203b1b1156b632e6da1d4dc47
Ian Hom <ianhom@163.com>
2016-09-14 08:49:25 +0800

    Create elif.py

---
+++ Note_1/elif.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+num = 5;
+if num == 3:
+    print "boss";
+elif num == 2:
+    print "user";
+elif num == 1:
+    print "worker";
+elif num < 0:
+    print "error";
+else:
+    print "roadman";
+print "end";
+
+# result
+"""
+roadman
+end
+"""
