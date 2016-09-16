a4ffd9ab3588a97d8c3ade60c327fc88fa77f1b5
Ian Hom <ianhom@163.com>
2016-09-16 11:50:45 +0800

    Create pass.py

---
+++ Note_1/pass.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+
+# 输出 Python 的每个字母
+for letter in "Python":
+   if letter == 'h':
+      pass;
+      print "这是 pass 块";
+   print "当前字母 :", letter
+
+print "End";
+
+# result
+"""
+当前字母 : P
+当前字母 : y
+当前字母 : t
+这是 pass 块
+当前字母 : h
+当前字母 : o
+当前字母 : n
+End
+"""
