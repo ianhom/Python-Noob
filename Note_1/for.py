9356ee27f314eb9a0191e7c4aeb40e592ce45a67
Ian Hom <ianhom@163.com>
2016-09-16 09:23:12 +0800

    Create for.py

---
+++ Note_1/for.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+for letter in "Python":
+    print "The letter is ", letter;
+	
+stake = ["stk100", "c4", "e3"];
+for s in stake:
+	print "It is ", s;
+print "end";
+
+# result
+"""
+The letter is P
+The letter is y
+The letter is t
+The letter is h
+The letter is o
+The letter is n
+It is stk100
+It is c4
+It is e3
+end
+"""
