133499f438f7766c996c90b54fae3ea9d05828e5
Ian Hom <ianhom@163.com>
2016-09-17 11:07:19 +0800

    Create string.py

---
+++ Note_1/string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+var1 = 'Hello World!'
+
+print "更新字符串 :- ", var1[:6] + 'Runoob!'
+print var1;  # var1 is NOT changed
+
+# result
+"""
+更新字符串 :- Hello Runoob!
+Hello World!
+"""
