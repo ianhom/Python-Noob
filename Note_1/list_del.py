1f70a7f9e013af1ec205cea48990279474f330f2
Ian Hom <ianhom@163.com>
2016-09-18 13:28:34 +0800

    Create list_del.py

---
+++ Note_1/list_del.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+list = ["hello", 123, 234, 456];
+print list;
+del list[2];
+print list;
+
+# result
+"""
+['hello', 123, 234, 456]
+['hello', 123, 456]
+"""
