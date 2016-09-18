1682be509ee65298bf943b9d22f5b8191e2fc679
Ian Hom <ianhom@163.com>
2016-09-18 13:35:15 +0800

    Create list_Op2.py

---
+++ Note_1/list_Op2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+list1 = [1,2,3,4,5,6,7,8,9,10];
+
+print list1[2];
+print list1[-2];   #倒数第二个数
+print list1[1:];
+print list1[:5];
+
+# result
+"""
+3
+9
+[2, 3, 4, 5, 6, 7, 8, 9, 10]
+[1, 2, 3, 4, 5]
+"""
