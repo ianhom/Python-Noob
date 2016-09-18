9cfe9399c20cc2b66beac45a427058b29e7eb642
Ian Hom <ianhom@163.com>
2016-09-18 13:52:00 +0800

    Update list_Op3.py

--- Note_1/list_Op3.py
+++ Note_1/list_Op3.py
@@ -10,6 +10,29 @@ print cmp(list2, list3)
 
 print max(list1);
 print min(list1);
+
+list1.append(123);  # 追加元素
+print list1;
+
+print list1.count(123);  # 统计有多少个123
+
+print list1.index("xyz"); # 返回第一个匹配的元素位置
+print list1.index(123);
+
+list1.insert(1,456);  # 插入一个元素
+print list1;
+print list1.pop(-2);  # 删除一个位置的元素
+print list1;
+list1.remove(123);    # 删除第一个匹配的元素
+print list1;
+list1.reverse();      # 反转list
+print list1;
+list1.reverse();     
+list1.insert(1,"sdfsdf");
+print list1;
+list1.sort();         # 排序list
+print list1;
+
 	
 # result
 """
@@ -18,4 +41,15 @@ print min(list1);
 -1
 xyz
 123
+[123, 'xyz', 123]
+2
+1
+0
+[123, 456, 'xyz', 123]
+xyz
+[123, 456, 123]
+[456, 123]
+[123, 456]
+[456, 'sdfsdf', 123]
+[123, 456, 'sdfsdf']
 """
