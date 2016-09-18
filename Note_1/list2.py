d3ce50a2195a10c8fb3268462795803e3d7ecc2e
Ian Hom <ianhom@163.com>
2016-09-18 13:26:23 +0800

    Create list2.py

---
+++ Note_1/list2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+list = ['physics', 'chemistry', 1997, 2000];
+
+print "Value available at index 2 : %d" % (list[2]);
+list[2] = 2001;
+print "Value available at index 2 : %d" % (list[2]);
+
+# result
+"""
+Value available at index 2 : 1997
+Value available at index 2 : 2001
+"""
