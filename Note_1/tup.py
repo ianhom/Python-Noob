32db989543f3b491aba43552fc3e240652b46ded
Ian Hom <ianhom@163.com>
2016-09-19 19:31:04 +0800

    Update tup.py

--- Note_1/tup.py
+++ Note_1/tup.py
@@ -3,6 +3,7 @@
 
 tup1 = (12, 34.56);
 tup2 = ('abc', 'xyz');
+list = [1,2,3,4,5];
 
 # 创建一个新的元组
 tup3 = tup1 + tup2;
@@ -16,6 +17,12 @@ print tup3[2];
 print tup3[-1];
 print tup3[1:];
 
+print cmp(tup1,tup2);
+print max(tup1,tup2);
+print min(tup1,tup2);
+print tuple(list);
+
+
 # 以下修改元组元素操作是非法的。
 tup1[0] = 100;
 
@@ -25,5 +32,9 @@ tup1[0] = 100;
 abc
 xyz
 (34.56, 'abc', 'xyz')
-Traceback (most recent call last): File "/usercode/file.py", line 22, in tup1[0] = 100; TypeError: 'tuple' object does not support item assignment 
+-1
+('abc', 'xyz')
+(12, 34.56)
+(1, 2, 3, 4, 5)
+Traceback (most recent call last): File "/usercode/file.py", line 27, in tup1[0] = 100; TypeError: 'tuple' object does not support item assignment 
 """
