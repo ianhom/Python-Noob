f0d60d014d3e7bc838d3e9cfd2effe6dd100b242
Ian Hom <ianhom@163.com>
2016-09-19 19:24:25 +0800

    Update tup.py

--- Note_1/tup.py
+++ Note_1/tup.py
@@ -8,11 +8,22 @@ tup2 = ('abc', 'xyz');
 tup3 = tup1 + tup2;
 print tup3;
 
+del tup3;
+
+tup3 = tup1 + tup2;
+
+print tup3[2];
+print tup3[-1];
+print tup3[1:];
+
 # 以下修改元组元素操作是非法的。
 tup1[0] = 100;
 
 # result
 """
 (12, 34.56, 'abc', 'xyz')
-Traceback (most recent call last): File "/usercode/file.py", line 12, in tup1[0] = 100; TypeError: 'tuple' object does not support item assignment 
+abc
+xyz
+(34.56, 'abc', 'xyz')
+Traceback (most recent call last): File "/usercode/file.py", line 22, in tup1[0] = 100; TypeError: 'tuple' object does not support item assignment 
 """
