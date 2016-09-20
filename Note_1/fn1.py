9a9030eb5afc39d8e27ab0c571c4369616323d63
Ian Hom <ianhom@163.com>
2016-09-20 08:49:07 +0800

    Create fn1.py

---
+++ Note_1/fn1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+# 定义函数
+def printme( str , i ):
+   print str ,i;
+   return;
+ 
+# 调用函数
+printme("我要调用用户自定义函数!",3);
+printme("再次调用同一函数",4);
+
+# result
+"""
+我要调用用户自定义函数! 3
+再次调用同一函数 4
+"""
