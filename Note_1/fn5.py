3489747e7483c6e6e8cb098c6f77ab1967ea13a5
Ian Hom <ianhom@163.com>
2016-09-20 22:36:12 +0800

    Create fn5.py

---
+++ Note_1/fn5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+total = 0; # 这是一个全局变量
+# 可写函数说明
+def sum( arg1, arg2 ):
+   #返回2个参数的和."
+   total = arg1 + arg2; # total在这里是局部变量.
+   print "函数内是局部变量 : ", total
+   return total;
+ 
+#调用sum函数
+print total;
+total = sum( 10, 20 );
+print "函数外是全局变量 : ", total 
+
+# result
+0
+函数内是局部变量 : 30
+函数外是全局变量 : 30
+"""
