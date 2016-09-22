a294a1e9b8a6d7bc3948db2ecce87fe4c49dd1e5
Ian Hom <ianhom@163.com>
2016-09-22 17:27:07 +0800

    Create io.py

---
+++ Note_1/io.py
@@ -0,0 +1,17 @@
+#!usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def add():
+    a = input("Please input a word:")
+    b = input("Please input another word:")
+    print(a+b)
+
+add();
+
+# result
+"""
+Please input a word:hello
+Please input another word: world!
+hello world!
+"""
+
