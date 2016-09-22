ad921d8c8c0598381a9b2df54956f8cb5743fbfd
Ian Hom <ianhom@163.com>
2016-09-22 17:06:07 +0800

    Create module2.py

---
+++ Note_1/module2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+Money = 2000
+def AddMoney():
+   # 想改正代码就取消以下注释:
+   global Money
+   Money = Money + 1
+ 
+print Money
+AddMoney()
+print Money
+
+# result
+"""
+2000
+2001
+"""
