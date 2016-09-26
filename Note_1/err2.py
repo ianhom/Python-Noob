605e319b874658b7dc7c828696477600216cde9b
Ian Hom <ianhom@163.com>
2016-09-26 13:26:26 +0800

    Create err2.py

---
+++ Note_1/err2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 定义函数
+def temp_convert(var):
+    try:
+        return int(var)
+    except ValueError, Argument:
+        print "参数没有包含数字\n", Argument
+
+# 调用函数
+temp_convert("ABCD");
+
+# result
+"""
+参数没有包含数字
+invalid literal for int() with base 10: 'ABCD'
+"""
