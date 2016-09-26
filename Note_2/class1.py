17de0351731c21a2e81ad4e46a09ca9cf7d7bb9a
Ian Hom <ianhom@163.com>
2016-09-26 13:36:53 +0800

    Create class1.py

---
+++ Note_2/class1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class dog:
+    'This is a dog class'
+    dogcnt = 1;
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age  = age
+        dog.dogcnt += 1
+      
+    def ShowCnt(self):
+        print "The count of dog is %d" % dog.dogcnt
+        
+    def ShowDog(self):
+        print "Name : ", self.name, ", Age : ", self.age
+        
+dog1 = dog("Mike",4)
+dog2 = dog("John",5)
+
+dog1.ShowCnt()
+dog2.ShowDog()
+print dog.dogcnt 
+
+# result
+"""
+The count of dog is 3
+Name : John , Age : 5
+3
+"""
