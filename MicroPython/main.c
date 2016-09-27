6160d36fed8a11546ae9beb38e3f61c4839081b8
Ian Hom <ianhom@163.com>
2016-09-27 12:39:41 +0800

    Update main.c

--- MicroPython/main.c
+++ MicroPython/main.c
@@ -1,26 +1,19 @@
-print('Hello, world, I am micropython')
-print('who are you!')
+print('***************************************************************')
+print('*            Hello, world, I am micropython ESP8266           *')
+print('***************************************************************')
 
 import time
-
+import os
 import machine
+
+ls = "Sorry! I am python, NOT shell!"
+sl = "Wowwow!!! I am a train~~:P"
+    
 p = machine.Pin(2,machine.Pin.OUT)
 p.high()
 
 def toggle(p):
     p.value(not p.value())
-
-toggle(p)
-time.sleep_ms(500)
-
-toggle(p)
-time.sleep_ms(500)
-
-toggle(p)
-time.sleep_ms(500)
-
-toggle(p)
-time.sleep_ms(500)
 
 def led(a,b,c):
     a *= 2
@@ -29,5 +22,7 @@ def led(a,b,c):
         time.sleep_ms(b)
         toggle(c)
     return
+     
+led(3,500,p) 
 
 
