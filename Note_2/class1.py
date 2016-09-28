0f31e242f3bce2b6a787e6cbbe34062b3cfd8c7b
Ian Hom <ianhom@163.com>
2016-09-28 15:35:31 +0800

    Update class1.py

--- Note_2/class1.py
+++ Note_2/class1.py
@@ -23,9 +23,20 @@ dog1.ShowCnt()
 dog2.ShowDog()
 print dog.dogcnt 
 
+print "dog.__doc__:", dog.__doc__
+print "dog.__name__:", dog.__name__
+print "dog.__module__:", dog.__module__
+print "dog.__bases__:", dog.__bases__
+print "dog.__dict__:", dog.__dict__
+
 # result
 """
 The count of dog is 3
 Name : John , Age : 5
 3
+dog.__doc__: This is a dog class
+dog.__name__: dog
+dog.__module__: __main__
+dog.__bases__: ()
+dog.__dict__: {'__module__': '__main__', 'ShowCnt': , 'dogcnt': 3, 'ShowDog': , '__doc__': 'This is a dog class', '__init__': }
 """
