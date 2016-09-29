782fa4697b6cf800b8def92e2510ef798b410bcd
Ian Hom <ianhom@163.com>
2016-09-29 13:20:01 +0800

    Create hello_win.py

---
+++ Note_1/hello_win.py
@@ -0,0 +1,7 @@
+print "Hello me!"
+#a = input("please input a number:")
+#b = input("Please input another:")
+#print a+b
+name = raw_input("What is your name?")
+print "hello " + name + "!"
+raw_input("PLease press <Enter>")
