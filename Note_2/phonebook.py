be60f885c3e28ca35162b4abe816fa89c009a75a
Ian Hom <ianhom@163.com>
2016-10-01 21:11:13 +0800

    Update phonebook.py

--- Note_2/phonebook.py
+++ Note_2/phonebook.py
@@ -29,5 +29,23 @@ request = raw_input('Phone number(p) or address(a)?');
 if request == 'p': key = 'phone';
 if request == 'a': key = 'addr';
 
-if name in people: print "%s's %s is %s." % (name, labels[key], people[name][key]);
+if name in people: print "%s's %s is %s." % (name, labels[key], people[name][key])
 else: print"Name is wrong!!";
+
+
+# result
+'''
+name: Beth
+Phone number(p) or address(a)?p
+Beth's phone number is 0123.
+
+name: Beth
+Phone number(p) or address(a)?a
+Beth's address is Foo street 03.
+
+name: Ian
+Phone number(p) or address(a)?p
+Name is wrong!!
+
+
+'''
