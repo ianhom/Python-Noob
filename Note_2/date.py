d37d2e94dff26f8e1d3c177f08e9148273186bad
Ian Hom <ianhom@163.com>
2016-10-02 20:07:05 +0800

    Update date.py

--- Note_2/date.py
+++ Note_2/date.py
@@ -31,3 +31,11 @@ month_name = months[month_number - 1];
 ordinal    = day + endings[day_number - 1];
 
 print month_name + " " + ordinal + "," + year 
+
+# result
+"""
+Year: 2016
+Month(1-12): 10
+Day(1-31): 2
+Oct 2nd,2016
+"""
