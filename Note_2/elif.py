cba7e8e445f17d86b63933d2efa05b67129f91aa
Ian Hom <ianhom@163.com>
2016-10-03 18:09:59 +0800

    Create elif.py

---
+++ Note_2/elif.py
@@ -0,0 +1,18 @@
+while True:
+    n = input('Enter a number: ');
+    if n > 0:
+        print "The number is positive";
+    elif n < 0:
+        print "The number is negtive";
+    else:
+        print "The number is 0";
+
+# result
+'''
+Enter a number: 1
+The number is positive
+Enter a number: 0
+The number is 0
+Enter a number: -2
+The number is negtive
+'''
