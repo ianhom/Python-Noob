4091ba1b6107e32602927d072172916f456cfa28
Ian Hom <ianhom@163.com>
2016-10-03 17:43:59 +0800

    Update loggin.py

--- Note_2/loggin.py
+++ Note_2/loggin.py
@@ -15,3 +15,17 @@ else:
     print "Wrong user name or PIN";
 
 
+# result
+'''
+User name: albert
+PIN code: 1234
+Access granted
+
+User name: albert
+PIN code: 4321
+Wrong user name or PIN
+
+User name: Ian
+PIN code: 1234
+Wrong user name or PIN
+'''
