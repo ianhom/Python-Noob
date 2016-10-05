b9293349ced75ff4c07f811bc4081097c6205677
Ian Hom <ianhom@163.com>
2016-10-05 20:02:01 +0800

    Create for.py

---
+++ Note_2/for.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+words = ["this","is","an","ex","parrot"];
+
+for word in words:
+    print word;
+
+print
+numbers = [0,1,2,3,4,5,6,7,8,9];
+
+for number in numbers:
+    print number;
+
+print
+
+for x in range(1,11):
+    print x;
+
+# result
+'''
+this
+is
+an
+ex
+parrot
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+'''
