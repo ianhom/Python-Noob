2a33e4990131bb507e47ef0ccf0be6f54fb02a03
Ian Hom <ianhom@163.com>
2016-10-06 10:02:32 +0800

    Create fordict.py

---
+++ Note_2/fordict.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+d = {'x':1, 'y':2, 'z':3};
+
+for key in d:
+    print key, "corresponds to ", d[key];
+
+# result
+'''
+y corresponds to  2
+x corresponds to  1
+z corresponds to  3
+'''
+
+# Why is y-x-z order???
