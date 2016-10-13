c25b45dfe68d625674232b5ee10e8f27c53e3f91
Ian Hom <ianhom@163.com>
2016-10-13 23:45:15 +0800

    Create class_person.py

---
+++ Note_2/class_person.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+__metaclass__ = type
+
+class Person:
+    def setName(self, name):
+        self.name = name;
+
+    def getName(self):
+        return self.name;
+
+    def greet(self):
+        print "Hello, world! I'm %s." % self.name;
+
+        
