5a8d6b897291d92241f673804097e96e4431f86d
Ian Hom <ianhom@163.com>
2016-10-16 20:07:48 +0800

    Update class_bird.py

--- Note_2/class_bird.py
+++ Note_2/class_bird.py
@@ -3,17 +3,24 @@
 class Bird:
     song = "Squaawk!"
     def sing(self):
+        "This is a function of bird singing."
         print self.song;
 
-bird = Bird()
-bird.sing()
+bird = Bird();
+print bird.sing.__doc__;
+bird.sing();
+print;
 
 birdsong = bird.sing
+print birdsong.__doc__;
 birdsong();
 
 # result
 '''
+This is a function of bird singing.
 Squaawk!
+
+This is a function of bird singing.
 Squaawk!
 '''
 
