577eec4d7400520252c588a3cbfd5fe4f540dcbf
Ian Hom <ianhom@163.com>
2016-10-17 23:04:03 +0800

    Update try.py

--- Note_2/try.py
+++ Note_2/try.py
@@ -7,12 +7,20 @@ try:
     print x/y;
 except ZeroDivisionError:
     print "The second number can not be zero!!";
+except TypeError:
+    print "That was not a number, was it?";
 
 # result
 '''
 Please enter a number: 1
 Please enter another number 2
+0
+
 Please enter a number: 1
 Please enter another number 0
 The second number can not be zero!!
+
+Please enter a number: 1
+Please enter another number "hi"
+That was not a number, was it?
 '''
