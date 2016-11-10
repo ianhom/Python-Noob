69e5ad808b87b506aef794c775a2b4cce42744c4
Ian Hom <ianhom@163.com>
2016-11-10 22:45:37 +0800

    Update file_read.py

--- Note_2/file_read.py
+++ Note_2/file_read.py
@@ -6,6 +6,9 @@ f = open("hi.txt","r");
 line = f.read(20);
 print "I get ", line;
 
+line = f.read(20);
+print "I get ", line;
+
 f.close();
 
 # result
@@ -13,4 +16,7 @@ f.close();
 I get  hello 1
 hello 2
 hell
+I get  o 3
+hello 4
+hello 5
 '''
