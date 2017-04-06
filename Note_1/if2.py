d8b9c90c58dccf3ca4fadaabeb359203c1e4c02f
Ian Hom <ianhom@163.com>
2017-04-06 08:25:01 +0800

    Create if2.py

---
+++ Note_1/if2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+userInput = raw_input("Enter 1 or 2: ")
+if userInput == "1":
+    print "Hello world"
+    print "How are you"
+elif userInput == "2":
+    print "Python Rocks!"
+    print "I love python"
+else:
+    print "Wrong"
+
+#result
+'''
+Enter 1 or 2: 1
+Hello world
+How are you
+
+Enter 1 or 2: 2
+Python Rocks!
+I love python
+
+Enter 1 or 2: 3
+Wrong
+'''
