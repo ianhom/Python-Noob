d65a76e1488b9b1b4ee5bd2665aa8a83897f3cdb
A byte of China <ianhom@163.com>
2017-06-29 16:15:38 +0800

    Create Example_26.py

---
+++ Note_3/Example_26.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+利用递归方法求5!。
+'''
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+def fact(j):
+    sum = 0
+    if j == 0:
+        sum = 1
+    else:
+        sum = j * fact(j - 1)
+    return sum
+ 
+for i in range(5):
+    print '%d! = %d' % (i,fact(i))
+
+# result
+'''
+0! = 1
+1! = 1
+2! = 2
+3! = 6
+4! = 24
+'''
