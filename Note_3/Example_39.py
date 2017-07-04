cf9816b0cbb0eca45899ee57b9730ce0f6e9d8c9
A byte of China <ianhom@163.com>
2017-07-04 14:18:52 +0800

    Create Example_39.py

---
+++ Note_3/Example_39.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：有一个已经排好序的数组。现输入一个数，要求按原来的规律将它插入数组中。
+'''
+ 
+if __name__ == '__main__':
+    # 方法一 ： 0 作为加入数字的占位符
+    a = [1,4,6,9,13,16,19,28,40,100,0]
+    print '原始列表:'
+    for i in range(len(a)):
+        print a[i],
+    number = int(raw_input("\n插入一个数字:\n"))
+    end = a[9]
+    if number > end:
+        a[10] = number
+    else:
+        for i in range(10):
+            if a[i] > number:
+                temp1 = a[i]
+                a[i] = number
+                for j in range(i + 1,11):
+                    temp2 = a[j]
+                    a[j] = temp1
+                    temp1 = temp2
+                break
+    print '排序后列表:'
+    for i in range(11):
+        print a[i],
+
+# result
+'''
+原始列表:
+1 4 6 9 13 16 19 28 40 100 0 
+插入一个数字:
+23
+排序后列表:
+1 4 6 9 13 16 19 23 28 40 100
+'''
