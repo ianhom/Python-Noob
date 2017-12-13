bba6fe6f9240ae4bd931952fe16db6ca560939ad
A Byte of China <ianhom@163.com>
2017-12-13 20:48:49 +0800

    Update get_remote_machine_info.py

--- App/web/get_remote_machine_info.py
+++ App/web/get_remote_machine_info.py
@@ -2,9 +2,13 @@ import socket
 
 def get_remote_machine_info():
     host_name = 'www.python.org'
-    ip_addr   = socket.gethostbyname(host_name)
-    print "Host name is %s" % host_name
-    print "The IP address is %s" % ip_addr
+    try:
+        ip_addr   = socket.gethostbyname(host_name)
+        print "Host name is %s" % host_name
+        print "The IP address is %s" % ip_addr
+    except socket.error, err_msg:
+        print "%s: %s" %(host_name, err_msg)
+          
 
 if __name__ == '__main__':
     get_remote_machine_info() 
