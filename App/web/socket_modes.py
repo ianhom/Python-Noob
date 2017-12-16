329271bea3e4c760d9396d4eeadbacf4997fdc3e
A Byte of China <ianhom@163.com>
2017-12-17 00:10:14 +0800

    Create socket_modes.py

---
+++ App/web/socket_modes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import socket
+
+def test_socket_modes():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setblocking(1)
+    s.bind("127.0.0.1",0)
+
+    socket_address = s.getsocketname()
+    print "Trivial Serever launched on socket : s" % str(socket_address)
+    while(1):
+        s.listen(1)
+
+if __name__ == '__main__':
+    test_socket_modes()
