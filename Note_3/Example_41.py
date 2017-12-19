1aa91d684fed924e2f58d8bfd8e862bef0bdfb0c
A Byte of China <ianhom@163.com>
2017-12-19 17:29:05 +0800

    Create Example_41.py

---
+++ Note_3/Example_41.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def varfunc():
+    var = 0
+    print 'var = %d' % var
+    var += 1
+if __name__ == '__main__':
+    for i in range(3):
+        varfunc()
+
+# 类的属性
+# 作为类的一个属性吧
+class Static:
+    StaticVar = 5
+    def varfunc(self):
+        self.StaticVar += 1
+        print self.StaticVar
+
+print Static.StaticVar
+a = Static()
+for i in range(3):
+    a.varfunc()
+
+# Result
+'''
+var = 0
+var = 0
+var = 0
+5
+6
+7
+8
+
+'''
