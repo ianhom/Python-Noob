a024b93dabb8cd32be6f87310ca3e8a6f6708898
A Byte of China <ianhom@163.com>
2017-12-21 13:00:03 +0800

    Create Example_50.py

---
+++ Note_3/Example_50.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：输出一个随机数。
+'''
+ 
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+import random
+ 
+#生成 123 到 456 之间的随机数
+print random.uniform(123,456)
+
+# Result
+'''
+296.322751698
+'''
