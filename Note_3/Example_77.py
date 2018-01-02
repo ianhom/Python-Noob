65c228cc80423cda939b3622a86d5e009460d088
A Byte of China <ianhom@163.com>
2018-01-02 19:57:46 +0800

    Create Example_77.py

---
+++ Note_3/Example_77.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：循环输出列表
+'''
+
+if __name__ == '__main__':
+    s = ["superman","wonder woman","Batman","Flash","cyber"]
+    for i in range(len(s)):
+        print s[i]
+
+
+# Result
+'''
+superman
+wonder woman
+Batman
+Flash
+cyber
+'''
