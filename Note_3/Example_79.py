8a44855aec995f4fd32342d8c9b547e92943c232
A Byte of China <ianhom@163.com>
2018-01-02 20:00:49 +0800

    Create Example_79.py

---
+++ Note_3/Example_79.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：字符串排序。
+'''
+
+if __name__ == '__main__':
+    str1 = raw_input('input string:\n')
+    str2 = raw_input('input string:\n')
+    str3 = raw_input('input string:\n')
+    print str1,str2,str3
+    
+    if str1 > str2 : str1,str2 = str2,str1
+    if str1 > str3 : str1,str3 = str3,str1
+    if str2 > str3 : str2,str3 = str3,str2
+ 
+    print 'after being sorted.'
+    print str1,str2,str3
+# Result
+'''
+input string:
+Hello world
+input string:
+hello
+input string:
+hell
+Hello world hello hell
+after being sorted.
+Hello world hell hello
+'''
