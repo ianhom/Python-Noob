bed2046a86a32a15b3c3624b7df0da83ac9a919c
A Byte of China <ianhom@163.com>
2018-01-03 09:14:38 +0800

    Create Example_84.py

---
+++ Note_3/Example_84.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：连接字符串。
+'''
+
+delimiter = ','
+mylist = ['Superman', 'Batman', 'Spiderman', 'X-man']
+print delimiter.join(mylist)
+    
+# Result
+'''
+Superman,Batman,Spiderman,X-man
+'''
