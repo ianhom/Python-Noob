c190508c80874ab90a08f0defc7ca35a60e06943
A Byte of China <ianhom@163.com>
2018-01-06 17:39:20 +0800

    Create Example_95.py

---
+++ Note_3/Example_95.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：字符串日期转换为易读的日期格式。
+'''
+
+from dateutil import parser
+dt = parser.parse("Aug 28 2015 12:00AM")
+print dt
+
+# Result
+'''
+2015-08-28 00:00:00
+'''
