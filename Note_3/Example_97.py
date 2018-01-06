2f7d4ad64a89884bc01d860d70864ed13da8787b
A Byte of China <ianhom@163.com>
2018-01-06 17:43:54 +0800

    Create Example_97.py

---
+++ Note_3/Example_97.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+题目：从键盘输入一些字符，逐个把它们写到磁盘文件上，直到输入一个 # 为止。
+'''
+
+if __name__ == '__main__':
+    from sys import stdout
+    filename = raw_input('输入文件名:\n')
+    fp = open(filename,"w")
+    ch = raw_input('输入字符串:\n')
+    while ch != '#':
+        fp.write(ch)
+        stdout.write(ch)
+        ch = raw_input('')
+    fp.close()
+
+# Result
+'''
+输入文件名:
+hello
+输入字符串:
+hello world
+hello worldhe
+hehey
+heyyou
+you#
+'''
