d448eeadf800d411d7da32614684412e7a027544
A Byte of China <ianhom@163.com>
2018-02-12 15:20:42 +0800

    Update CMG.py

--- App/CodeModuleGenerator/CMG.py
+++ App/CodeModuleGenerator/CMG.py
@@ -4,18 +4,6 @@
 from time import *
 from os   import mkdir,chdir,getcwd
 
-common_file_head= '''/******************************************************************************
-* Copyright (C) 2018 MOE
-* Name   : To be done
-* ID     : None
-* Descrp : To be done
-* Version: V1.00
-* Author :      
-* Date   ：To be done
-******************************************************************************/'''
-
-
-
 # Create folder named by time.
 timestr =  strftime("%Y%m%d-%H%M%S", localtime())
 
@@ -24,11 +12,11 @@ if getcwd()[-11:] != "Code Module":
     chdir("Code Module")
 print "/_/_/_/_/_/_/_/_/_/_/_/_/_//_/_/_/_/"
 print "Code Module Generator V0.10"
-print "/_/_/_/_/_/_/_/_/_/_/_/_/_//_/_/_/_\n"
+print "/_/_/_/_/_/_/_/_/_/_/_/_/_//_/_/_/_/\n"
 
-Author_name = raw_input("May I have you name?")
-module_name = raw_input("What would like to call this module?")
-module_desp = raw_input("Please say something about this module")
+Author_name = raw_input("May I have you name?\n")
+module_name = raw_input("What would like to call this module?\n")
+module_desp = raw_input("Please say something about this module\n")
 
 mkdir(module_name + timestr)
 chdir(module_name + timestr)
@@ -37,20 +25,31 @@ c_file = open(module_name + '.c', "w+")
 h_file = open(module_name + '.h', "w+")
 
 file_name = "* Name   : "+ module_name + ".c\n"
+file_Cprt = "* Copyright (C) 2018 MOE\n"
 file_ID   = "* ID     : N/A\n"
 file_desp = "* Descrp : "+ module_desp+"\n"
 file_ver  = "* Version: V1.00\n"
 file_Auth = "* Author : "+ Author_name+"\n"
 file_date = "* Date   ："+ timestr[0:8]+"\n"
 banner_bg = "/***************************************\n"
-banner_ed = "***************************************/\n"
+banner_ed = "***************************************/\n\n"
 
-file_head = banner_bg + file_name + file_ID + file_desp + file_ver + file_Auth + file_date + banner_ed
-c_file.write(file_head)
+file_head = banner_bg + file_Cprt + file_name + file_ID + file_desp + file_ver + file_Auth + file_date + banner_ed
+
+file_init = "BYTE " + module_name + "_Init(void)\n{\n\n    return SW_OK;\n}\n\n"
+file_proc = "BYTE " + module_name + "_Process(BYTE ucChNo)\n{\n\n    return SW_OK;\n}\n\n"
+
+file_all = file_head + file_init + file_proc
+c_file.write(file_all)
 
 file_name = "* Name   : "+ module_name + ".c\n"
-file_head = banner_bg + file_name + file_ID + file_desp + file_ver + file_Auth + file_date + banner_ed
-h_file.write(file_head)
+file_head = banner_bg + file_Cprt + file_name + file_ID + file_desp + file_ver + file_Auth + file_date + banner_ed
+
+file_init = "BYTE " + module_name + "_Init(void);\n\n"
+file_proc = "BYTE " + module_name + "_Process(BYTE ucChNo);\n\n"
+
+file_all = file_head + file_init + file_proc
+h_file.write(file_all)
 
 # return
 c_file.close()
