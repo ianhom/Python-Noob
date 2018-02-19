8bd368370e3d76a20178e3f82d652b2caaee1558
A Byte of China <ianhom@163.com>
2018-02-19 08:48:38 +0800

    Update common.py

--- App/CodeModuleGenerator/common.py
+++ App/CodeModuleGenerator/common.py
@@ -1,12 +1,16 @@
 # Common defines for CMG
 
 # MACRO
-mif   = "#if"
-mels  = "#else"
-mend  = "#end"
+mif   = "#if "
+mels  = "#else "
+mend  = "#end "
 mdf   = "#define "
-mifd  = "#ifdef"
-mifnd = "#ifndef"
+mifd  = "#ifdef "
+mifnd = "#ifndef "
+minc  = "#include "
+mpck1 = "#pack(1)"
+mpck4 = "#pack(4)"
+mpop  = "#pop"
 
 
 # End of file and make sure there is a blank line
