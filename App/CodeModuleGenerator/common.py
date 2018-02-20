60d66f98c015c38a9aba483f6138199bd6dcd17a
A Byte of China <ianhom@163.com>
2018-02-20 21:48:01 +0800

    Update common.py

--- App/CodeModuleGenerator/common.py
+++ App/CodeModuleGenerator/common.py
@@ -5,12 +5,14 @@ mif   = "#if "
 mels  = "#else "
 mend  = "#end "
 mdf   = "#define "
+mundf = "#undef "
 mifd  = "#ifdef "
 mifnd = "#ifndef "
 minc  = "#include "
 mpck1 = "#pack(1)"
 mpck4 = "#pack(4)"
 mpop  = "#pop"
+mpush = "#push"
 
 
 # End of file and make sure there is a blank line
